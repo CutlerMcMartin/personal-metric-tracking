7a9724213daa4a27cc45925ab54ad967e188fff3
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-03-30 20:28:41 -0500

    Fixing the date format

--- webdesignforums/wdf-scraper.py
+++ webdesignforums/wdf-scraper.py
@@ -6,9 +6,10 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser') ## Making the Beautiful Soup o
 
 scraped_info = soup.select('.forum_post .content')
 
-data_array = []
+thread_array = []
 for row in scraped_info:
-    date_string = row.select('.hasavatar .datetime .date')[0].text.strip().split('\xa0')[0][:-1]
-    data_array.append([row.select('.hasavatar .title')[0].text.strip(), date_string])
+    date_array = row.select('.hasavatar .datetime .date')[0].text.strip().split(',')
+    date_string = date_array[0][:-2] + ',' + date_array[1]
+    thread_array.append([row.select('.hasavatar .title')[0].text.strip(), date_string])
 
-print(data_array[5])
+print(thread_array[5])
