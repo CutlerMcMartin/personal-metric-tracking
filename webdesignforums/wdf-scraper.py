81507792ab958f2a105e4102083d98be3ed0240a
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-04-02 23:53:05 -0500

    Changing Date format and adding json

--- webdesignforums/wdf-scraper.py
+++ webdesignforums/wdf-scraper.py
@@ -1,4 +1,5 @@
-import csv, webbrowser, sys, requests, bs4, re
+import csv, webbrowser, sys, requests, bs4, json
+from datetime import datetime
 
 res = requests.get('http://www.webdesignforums.net/forum/members/cutler.html?tab=activitystream&type=user#activitystream')
 
@@ -6,10 +7,20 @@ soup = bs4.BeautifulSoup(res.text, 'html.parser') ## Making the Beautiful Soup o
 
 scraped_info = soup.select('.forum_post .content')
 
+## Getting array of all the threads from the website
 thread_array = []
 for row in scraped_info:
     date_array = row.select('.hasavatar .datetime .date')[0].text.strip().split(',')
     date_string = date_array[0][:-2] + ',' + date_array[1]
+    date_string = datetime.strptime(date_string, '%b %d, %Y').strftime('%m/%d/%Y')
     thread_array.append([row.select('.hasavatar .title')[0].text.strip(), date_string])
 
-print(thread_array[5])
+    ##print(thread_array[5])
+
+## Converting Existing JSON file into a dict
+with open('webdesignforums/wdf-threads.json') as f:
+  old_threads = json.load(f)
+
+print(old_threads)
+
+## 
