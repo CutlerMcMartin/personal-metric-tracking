5a4242278e5f0e5d9c7853ba23c4487149f5e794
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-04-03 23:35:18 -0500

    Converting json old threads to array for intersection

--- webdesignforums/wdf-scraper.py
+++ webdesignforums/wdf-scraper.py
@@ -10,6 +10,8 @@ scraped_info = soup.select('.forum_post .content')
 ## Getting array of all the threads from the website
 thread_array = []
 for row in scraped_info:
+    ## Convert "Today" and "Yesterday" posts
+
     date_array = row.select('.hasavatar .datetime .date')[0].text.strip().split(',')
     date_string = date_array[0][:-2] + ',' + date_array[1]
     date_string = datetime.strptime(date_string, '%b %d, %Y').strftime('%m/%d/%Y')
@@ -21,6 +23,16 @@ for row in scraped_info:
 with open('webdesignforums/wdf-threads.json') as f:
   old_threads = json.load(f)
 
-print(old_threads)
+## Making a list of Titles
+old_thread_array = []
+for thread in old_threads['Threads']:
+  old_thread_array.append(thread["title"])
+print(old_thread_array)
 
-## 
+## Left ntersect the arrays
+
+
+
+## Any leftover gets added to the JSON string
+
+## Remake the JSON file
